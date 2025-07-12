4216e1e049080bf449af3950cbf2fe2d76f08993
Lucas Guimarães <guimaraes.lucas@gmail.com>
2025-07-11 21:18:27 -0300

    Update trumbowyg.intellisense.js to include license

--- intellisense/trumbowyg.intellisense.js
+++ intellisense/trumbowyg.intellisense.js
@@ -1,3 +1,31 @@
+/**
+ * Trumbowyg - intellisense Plugin
+ *
+ * This plugin enhances the editor by simulating an IntelliSense-like feature. It allows a
+ * suggestion box to be triggered by a specific character (e.g., /), fetching dynamic content
+ * from a backend endpoint and inserting it directly into the editor. It's perfect for
+ * creating templates, canned responses, or dynamic snippets.
+ * Trumbowyg is a program by <alex-d.github.io/Trumbowyg>
+ *
+ * @author  Lucas Guimarães <https://github.com/guimaraeslucas/>
+ *
+ * Copyright 2025, Lucas Guimarães e G3Pix Ltda <https://g3pix.com.br>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
 (function ($) {
     'use strict';
 
